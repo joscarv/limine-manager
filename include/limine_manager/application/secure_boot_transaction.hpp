07a66b5107dbf2a48b5a3415f9eee09ac6c78af4
joscarv <102634530+joscarv@users.noreply.github.com>
2026-07-20 19:06:00 -0600

    Add destructor rollback reporter to secure boot transaction

--- include/limine_manager/application/secure_boot_transaction.hpp
+++ include/limine_manager/application/secure_boot_transaction.hpp
@@ -5,13 +5,16 @@
 
 #include <filesystem>
 #include <optional>
+#include <string_view>
 
 namespace limine_manager::application {
 
 class SecureBootTransaction {
   public:
-    explicit SecureBootTransaction(std::filesystem::path efi_image);
-    ~SecureBootTransaction();
+    explicit SecureBootTransaction(
+        std::filesystem::path efi_image,
+        infrastructure::RollbackErrorReporter error_reporter = {});
+    ~SecureBootTransaction() noexcept;
 
     SecureBootTransaction(const SecureBootTransaction &) = delete;
     SecureBootTransaction &operator=(const SecureBootTransaction &) = delete;
@@ -25,8 +28,10 @@ class SecureBootTransaction {
 
   private:
     void rollback_config();
+    void report_destructor_error(std::string_view message) const noexcept;
     [[nodiscard]] bool rollback_pending() const noexcept;
 
+    infrastructure::RollbackErrorReporter error_reporter_;
     infrastructure::EfiImageTransaction efi_transaction_;
     std::optional<ApplyResult> apply_result_;
     bool config_rollback_pending_{false};
